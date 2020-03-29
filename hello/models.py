cc82b00d9deae587958e5ad127a73c8f735016d5
c-bata <contact@c-bata.link>
2020-03-29 19:32:45 +0900

    Add choice2 column

--- hello/models.py
+++ hello/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     choice1 = models.CharField(max_length=20, default='')
+    choice2 = models.CharField(max_length=20, default='')
