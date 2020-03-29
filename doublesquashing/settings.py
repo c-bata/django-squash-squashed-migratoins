ad8791a67e68de49697fc62361910995829e465d
c-bata <contact@c-bata.link>
2020-03-29 19:06:41 +0900

    Add question model

--- doublesquashing/settings.py
+++ doublesquashing/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
+    'hello.apps.HelloConfig',
 ]
 
 MIDDLEWARE = [
