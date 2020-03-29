2a0eb45b55d8a921fcac6f3bcc4d97a123ba0ad0
c-bata <contact@c-bata.link>
2020-03-29 19:34:43 +0900

    Add choice3 column

--- hello/models.py
+++ hello/models.py
@@ -5,3 +5,4 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     choice1 = models.CharField(max_length=20, default='')
     choice2 = models.CharField(max_length=20, default='')
+    choice3 = models.CharField(max_length=20, default='')
